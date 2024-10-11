e5eeccf7e80620455ccfd69df9cbfbd4066a74a2
Marco Antonio Arruda Da Silva <maanton2@c1r1p3.42sp.org.br>
2024-10-11 00:58:17 -0300

    FEAT: Create headers

---
+++ libft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/11 00:13:19 by maanton2          #+#    #+#             */
+/*   Updated: 2024/10/11 00:13:32 by maanton2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+int	ft_isalpha(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_isalnum(int c);
+int	ft_isdigit(int c);
+
+#endif
