26cd15f832c8962da02687ff2bc8805fc5f3a2f5
Marco Arruda <marcoantonioarrudadasilva@gmail.com>
2024-10-13 11:42:31 -0300

    UPDATE: add strncmp

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 00:13:19 by maanton2          #+#    #+#             */
-/*   Updated: 2024/10/12 19:06:48 by maanton2         ###   ########.fr       */
+/*   Updated: 2024/10/13 11:38:31 by maanton2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,6 @@ int		ft_toupper(int c);
 size_t	ft_strlen(const char *s);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
 
 #endif
