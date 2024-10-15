0834e978d01c998759d8c1cce83d4b1f0f9e2c2b
Marco Antonio Arruda Da Silva <maanton2@c1r3p2.42sp.org.br>
2024-10-15 00:18:27 -0300

    FEAT: Replay function memchr string

---
+++ ft_memchr.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 11:48:38 by maanton2          #+#    #+#             */
+/*   Updated: 2024/10/15 00:04:39 by maanton2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	const unsigned char		*target;
+	unsigned char			ch;
+
+	target = (const unsigned char *)s;
+	ch = (unsigned char)c;
+	while (n--)
+	{
+		if (*target == ch)
+			return (void *)target;
+		target++;
+	}
+	return (NULL);
+}
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 00:13:19 by maanton2          #+#    #+#             */
-/*   Updated: 2024/10/14 21:43:47 by maanton2         ###   ########.org.br   */
+/*   Updated: 2024/10/15 00:05:45 by maanton2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,19 @@
 # include <stdio.h>
 
 void		ft_bzero(void *s, size_t len);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
+int			ft_isalnum(int c);
+int			ft_isalpha(int c);
+int			ft_isascii(int c);
+int			ft_isdigit(int c);
+int			ft_isprint(int c);
+int			ft_tolower(int c);
+int			ft_toupper(int c);
 size_t		ft_strlen(const char *s);
 char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
-int		ft_strncmp(const char *str1, const char *str2, size_t n);
+int			ft_strncmp(const char *str1, const char *str2, size_t n);
 void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+
 
 #endif
