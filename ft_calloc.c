71b9c1fa6511aa8c5791ea28efaf8d06dd78d502
Marco Arruda <maanton2@student.42sp.org.br>
2024-10-17 17:07:46 -0300

    FEAT: Replay function calloc lib stdlib

---
+++ ft_calloc.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/13 11:48:38 by maanton2          #+#    #+#             */
+/*   Updated: 2024/10/17 15:13:11 by maanton2         ###   ########.org.br   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*p;
+	size_t	total_size;
+
+	total_size = nmemb * size;
+	p = malloc(total_size);
+	if(p == NULL)
+		return (NULL);
+	ft_bzero(p,total_size);
+	return (p);
+}
