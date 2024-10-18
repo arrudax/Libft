53dd845648a12e46fd128db0d3f69e1c530d8bb2
Marco Arruda <maanton2@student.42sp.org.br>
2024-10-18 18:09:18 -0300

    UPDATE: strjoin strtrim

--- Makefile
+++ Makefile
@@ -1,27 +1,30 @@
-NAME 				= libft.a
+NAME				= libft.a
 
+NAME_EXEC_TEST		= run_test
 
-SOURCE_FILES 		= ft_isalnum.c \
-	ft_atoi.c \
+SOURCE_FILES		= ft_atoi.c \
 	ft_bzero.c \
 	ft_calloc.c \
+	ft_isalnum.c \
 	ft_isalpha.c \
 	ft_isascii.c \
 	ft_isdigit.c \
 	ft_isprint.c \
 	ft_memchr.c \
-	ft_memcpy.c \
 	ft_memcmp.c \
+	ft_memcpy.c \
 	ft_memmove.c \
 	ft_memset.c \
 	ft_strchr.c \
 	ft_strdup.c \
+	ft_strjoin.c \
 	ft_strlcat.c \
 	ft_strlcpy.c \
 	ft_strlen.c \
 	ft_strncmp.c \
 	ft_strnstr.c \
 	ft_strrchr.c \
+	ft_strtrim.c \
 	ft_tolower.c \
 	ft_toupper.c 
 
@@ -37,15 +40,14 @@ SOURCE_FILES_TEST	= tests/test_ft_isalpha.c \
 	tests/test_ft_toupper.c \
 	tests/custom_print.c
 
-NAME_EXEC_TEST	= run_test
-SOURCE_DIR 	= .
-INCS 		= .
-
-CCPFLAGS	= -I $(INCS)
+SOURCE_DIR		= .
+INCS			= .
+CCPFLAGS		= -I $(INCS)
 CCPFLAGS_TEST	:= -I$$HOME/.local/include -L$$HOME/.local/lib/x86_64-linux-gnu
+
 OBJECT_FILES 	= $(SOURCE_FILES:.c=.o)
 
-CC 		= cc
+CC 			= cc
 CFLAGS		= -Wall -Werror -Wextra
 
 all: $(NAME)
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/11 00:13:19 by maanton2          #+#    #+#             */
-/*   Updated: 2024/10/17 15:16:56 by maanton2         ###   ########.org.br   */
+/*   Updated: 2024/10/18 15:27:07 by maanton2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,22 @@ int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int c);
+void		*ft_memchr(const void *s, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+char		*ft_strchr(const char *s, int c);
+char		*ft_strdup(const char *s);
+char		*ft_strjoin(char const *s1, char const *s2);
+size_t		ft_strlcat(char *dest, const char *src, size_t size);
+size_t		ft_strlcpy(char *dest, const char *src, size_t size);
+size_t		ft_strlen(const char *s);
+int			ft_strncmp(const char *str1, const char *str2, size_t n);
+char		*ft_strnstr(const char *big, const char *little, size_t len);
+char		*ft_strrchr(const char *s, int c);
+char		*ft_strtrim(char const *s, char const *set);
 int			ft_tolower(int c);
 int			ft_toupper(int c);
-size_t		ft_strlen(const char *s);
-char		*ft_strchr(const char *s, int c);
-char		*ft_strrchr(const char *s, int c);
-int			ft_strncmp(const char *str1, const char *str2, size_t n);
-void		*ft_memset(void *s, int c, size_t n);
-void		*ft_memchr(const void *s, int c, size_t n);
-void		*ft_memcpy(void *dest, const void *src, size_t n);
-int			ft_memcmp(const void *s1, const void *s2, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t n);
-char		*ft_strnstr(const char *big, const char *little, size_t len);
-size_t		ft_strlcpy(char *dest, const char *src, size_t size);
-size_t		ft_strlcat(char *dest, const char *src, size_t size);
-char		*ft_strdup(const char *s);
+
 #endif
