e226ae24d4c5427844c3a64e5e7a2e09022efb7d
Marco Arruda <maanton2@student.42sp.org.br>
2024-10-19 12:38:01 -0300

    FEAT: FIX's

---
+++ fix.c
@@ -0,0 +1,9 @@
+ft_memchr *target != ch \ *target++ != ch
+ft_memcmp *p_1 != !p_2 \ *p_1++ != *p_2++
+ft_memmove unsigned char \ char
+ft_strchr *s == (char)c \ *s++
+ft_strrchr *s == (char)c \ *s++
+ft_strlcpy remover {}
+ft_strlen s[i] != '\0' \ !s
+ft_strcmp trocar index por ponteiro
+ft_strnstr *p_b != == '\0' \ !p_b
