116e36b7102120fcafadf1a2749d23a45b3ff0f1
Marco Arruda <maanton2@student.42sp.org.br>
2024-10-23 11:27:00 -0300

    FIX: Norminette

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/13 11:48:38 by maanton2          #+#    #+#             */
-/*   Updated: 2024/10/16 23:00:56 by maanton2         ###   ########.org.br   */
+/*   Updated: 2024/10/21 23:26:28 by maanton2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,27 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	p_s = (unsigned char *)src;
 	if (p_d == p_s || n == 0)
 		return (dest);
-	if (p_d < p_s)
+	else if (p_d < p_s)
 		while (n--)
 			*p_d++ = *p_s++;
 	else
 	{
-	p_d += n;
+		p_d += n;
 		p_s += n;
 		while (n--)
 			*--p_d = *--p_s;
 	}
 	return (dest);
 }
+/*
+int main() {
+    char str[] = "josedasilva";  // String original
+
+    printf("Antes do memmove, str = %s\n", str);
+
+    ft_memmove(str + 2, str, 5);
+
+    printf("\nDepois do memmove (com sobreposição), str = %s\n", str);
+
+    return 0;
+}*/
